2d035c35f1f16d2a3f17040697b65c863bae1ac8
Technocrator <69451815+Technocrator@users.noreply.github.com>
2023-06-23 14:37:20 +0300

    23.06.2023
    С обученной моделью. Отделяет манетку от неманетки.

--- untitled0.py
+++ untitled0.py
@@ -5,7 +5,7 @@ Created on Wed Jun 21 14:05:24 2023
 @author: 0
 """
 
-from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing.image import ImageDataGenerator
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Conv2D, MaxPooling2D
 from tensorflow.python.keras.layers import Activation, Dropout, Flatten, Dense
@@ -31,3 +31,60 @@ nb_train_samples = 3659
 nb_validation_samples = 784
 # Количество изображений для тестирования
 nb_test_samples = 784
+
+model = Sequential()
+model.add(Conv2D(32, (3, 3), input_shape=input_shape))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+
+datagen = ImageDataGenerator(rescale=1. / 255)
+
+train_generator = datagen.flow_from_directory(
+    train_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='binary')
+
+val_generator = datagen.flow_from_directory(
+    val_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='binary')
+
+test_generator = datagen.flow_from_directory(
+    test_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='binary')
+
+model.fit_generator(
+    train_generator,
+    steps_per_epoch=nb_train_samples // batch_size,
+    epochs=epochs,
+    validation_data=val_generator,
+    validation_steps=nb_validation_samples // batch_size)
+
+scores = model.evaluate_generator(test_generator, nb_test_samples // batch_size)
+print("Аккуратность на тестовых данных: %.2f%%" % (scores[1]*100))
+
+model.save_weights('first_model_weights.h5')
+model.save('coins_model_1')
