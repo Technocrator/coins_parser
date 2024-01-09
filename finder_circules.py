79535f9f9250166fc6b1a872f8965d57972c79d0
Technocrator <69451815+Technocrator@users.noreply.github.com>
2024-01-09 18:00:40 +0300

    09.01.2024

--- finder_circules.py
+++ finder_circules.py
@@ -2,20 +2,21 @@ import cv2
 import numpy as np 
 
 image = cv2.imread("D:\\python\\coins_parser\\pictures_dataset\\10_kop_silver_sorted\\test\\coins\\coins1.jpg") 
-#сначала переведём изображение в оттенки серого
-gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-#заблурим изображение, т.е. сделаем размытым
+#Фильтрация изображения
+gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY) 
 blur = cv2.GaussianBlur(gray, (5, 5), 0) 
-cv2.imshow('gray', blur)
-#cv2.threshold делает изображение чернобелым возвращая список данных из которого нам нужен элемент с индексом 1. 
-thresh = cv2.threshold(blur, 0, 255,cv2.THRESH_BINARY_INV|cv2.THRESH_OTSU)[1]
-
-
-circles = cv2.HoughCircles(thresh,cv2.HOUGH_GRADIENT,1,20, param1=50,param2=30,minRadius=0,maxRadius=0)
-
-print (circles)
-
-# for i in circles:
-#     cv2.circle(image,(i[0],i[1]),i[2],(0,0,255),2)
-# cv2.imshow('Image', image)
-# cv2.waitKey(0)
+thresh = cv2.threshold(blur, 0, 255,
+cv2.THRESH_BINARY_INV|cv2.THRESH_OTSU)[1] 
+# circles = cv2.HoughCircles(thresh,cv2.HOUGH_GRADIENT,1,20,param1=50,param2=30,minRadius=0,maxRadius=0)[0]
+circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 8,
+                               param1=100, param2=30,
+                               minRadius=1, maxRadius=30)
+if circles is not None:
+        circles = np.uint16(np.around(circles))
+        for i in circles[0, :]:
+            center = (i[0], i[1])
+            # circle center
+            cv2.circle(src, center, 1, (0, 100, 100), 3)
+            # circle outline
+            radius = i[2]
+            cv2.circle(src, center, radius, (255, 0, 255), 3)
